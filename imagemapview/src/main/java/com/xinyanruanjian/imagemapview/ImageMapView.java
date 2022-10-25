91fa48e19b28210b30f1d003aa26c0788396c22b
sahooz <2423096504@qq.com>
2022-10-25 11:45:04 +0800

    add map size getter&setter

--- imagemapview/src/main/java/com/xinyanruanjian/imagemapview/ImageMapView.java
+++ imagemapview/src/main/java/com/xinyanruanjian/imagemapview/ImageMapView.java
@@ -62,6 +62,22 @@ public class ImageMapView extends AppCompatImageView {
         }
     }
 
+    public int getMapWidth() {
+        return mapWidth;
+    }
+
+    public void setMapWidth(int mapWidth) {
+        this.mapWidth = mapWidth;
+    }
+
+    public int getMapHeight() {
+        return mapHeight;
+    }
+
+    public void setMapHeight(int mapHeight) {
+        this.mapHeight = mapHeight;
+    }
+
     @Override
     public void setOnClickListener(@NonNull OnClickListener l) {
         if(null == l) {
